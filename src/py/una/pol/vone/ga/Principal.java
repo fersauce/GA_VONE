22d80f13d86661aff5db00c3b4eaaee65193fd5f
Alcides Rivarola <aerivarola@gmail.com>
2016-10-18 20:35:33 -0300

    Correcion path para el grafo

--- src/py/una/pol/vone/ga/Principal.java
+++ src/py/una/pol/vone/ga/Principal.java
@@ -27,13 +27,13 @@ public class Principal {
 		String graphFileName;
 		int K;
 		
-		graphFileName = "GA_VONE/src/py/una/pol/vone/ga/USNet.txt";
+		graphFileName = "src/py/una/pol/vone/ga/USNet.txt";
 		K = 6;
 		//
 		List<Camino> todosLosCaminos = hallarKCaminos(graphFileName, K);
 		//TODO Esquematizar la red sustrato con todos los FS (4000 para 50 THz), representacion del vone de manera 
 		//computacional
-		//TODO Algortimo de generación de redes virtuales
+		//TODO Algortimo de generaciï¿½n de redes virtuales
 		//TODO Algoritmo Genetico debe ir aqui.
 		//TODO Mostrar resultados, hacer comparativas y recibirnos de unos malditos ingenieros.
 	}
@@ -55,7 +55,7 @@ public class Principal {
 		long tiempoInicial = System.currentTimeMillis();
 		for(int origen = 0; origen < cantidadNodos; origen++){
 			for(int destino = origen+1;destino < cantidadNodos; destino++){
-				//Aquí se halla el camino entre origen y destino.
+				//Aquï¿½ se halla el camino entre origen y destino.
 				List<Path> caminos;
 				Yen algoritmoYen = new Yen();
 				
@@ -71,9 +71,9 @@ public class Principal {
 		}
 		long tiempoFinal = System.currentTimeMillis();
 		System.out.println("Completado.");
-		System.out.println("La búsqueda se ha realizado en "+(tiempoFinal-tiempoInicial)/1000.00+" segundos.");
+		System.out.println("La bï¿½squeda se ha realizado en "+(tiempoFinal-tiempoInicial)/1000.00+" segundos.");
 		for (Camino camino:todosLosCaminos){
-			System.out.println("A continuación mostramos los caminos hallados");
+			System.out.println("A continuaciï¿½n mostramos los caminos hallados");
 			System.out.println("k) costo: [camino]");
 			int n = 0;
 			for(Path p:camino.getCaminos()){
